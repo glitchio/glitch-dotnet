d7c920544e9d5629414d4b66289a7daefc0f982a
Javi <javiercrespoalvez@gmail.com>
2013-02-19 21:53:48 +0100

    web api error filter

---
+++ Glitch.Notifier.AspNet/Http/GlitchExceptionFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http.Filters;
+
+namespace Glitch.Notifier.AspNet.Http
+{
+    public class GlitchWebApiHandleErrorAttribute:ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            Glitch.Notify(actionExecutedContext.Exception)
+                 .WithCurrentUser()
+                 .WithHttpHeaders()
+                 .WithQueryString()
+                 .WithUrl()
+                 .With("Controller", actionExecutedContext.ActionContext.ControllerContext.RouteData.Values["controller"])
+                 .With("Action", actionExecutedContext.ActionContext.ControllerContext.RouteData.Values["action"])
+                 .WithErrorProfile("v1.net.webapi")
+                 .Send();
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
