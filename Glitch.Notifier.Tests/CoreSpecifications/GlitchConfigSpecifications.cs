5050652addba59ce2cf3b5117283c39d30ad43e8
javicrespo <javiercrespoalvez@gmail.com>
2013-02-26 16:26:16 +0100

    glitch config specs

---
+++ Glitch.Notifier.Tests/CoreSpecifications/GlitchConfigSpecifications.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Glitch.Notifier.Tests.CoreSpecifications
+{
+    [TestClass]
+    public class GlitchConfigSpecifications
+    {
+        [TestMethod]
+        public void Given_https_is_enabled_Should_reflect_it_in_the_url()
+        {
+            Glitch.Config.UseHttps(true);
+
+            Glitch.Config.Url.StartsWith("https");
+        }
+
+        [TestMethod]
+        public void Given_https_is_not_enabled_Should_reflect_it_in_the_url()
+        {
+            Glitch.Config.UseHttps(false);
+
+            Glitch.Config.Url.StartsWith("http");
+        }
+    }
+}
